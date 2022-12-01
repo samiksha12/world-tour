d021125c56e7a728ea68e42c6fed9e53ee257d48
samiksha12 <sapkota.samiksha@gmail.com>
2022-12-01 12:05:53 -0600

    world tour site update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,32 @@
-var countryElements = document.getElementById("countries").childNodes;
-var countryCount = countryElements.length;
+var continentElements = document.getElementsByClassName("continents");
+var countryCount = continentElements.length;
+var africa = document.getElementById("africa");
+var asia = document.getElementById("asia");
+var namerica = document.getElementById("namerica");
+var samerica = document.getElementById("samerica");
+var europe = document.getElementById("europe");
+var australia = document.getElementById("australia");
 for (var i = 0; i < countryCount; i++) {
-  countryElements[i].onmouseover = function () {
+  continentElements[i].onclick = function () {
     // alert("You clicked on " + this.getAttribute("data-name"));
-    const title= document.createElement("title");
-    title.innerText=this.getAttribute("data-name");
-    this.appendChild(title);
+    if(this.getAttribute("data-name")=="Africa"){
+      africa.scrollIntoView({behavior: "smooth",block: 'center'});
+    }
+    if(this.getAttribute("data-name")=="Asia"){
+      asia.scrollIntoView({behavior: "smooth",block: 'center'});
+    }
+    if(this.getAttribute("data-name")=="Europe"){
+      europe.scrollIntoView({behavior: "smooth",block: 'center'});
+    }
+    if(this.getAttribute("data-name")=="North America"){
+      namerica.scrollIntoView({behavior: "smooth",block: 'center'});
+    }
+    if(this.getAttribute("data-name")=="South America"){
+      samerica.scrollIntoView({behavior: "smooth",block: 'center'});
+    }
+    if(this.getAttribute("data-name")=="Australia"){
+      australia.scrollIntoView({behavior: "smooth",block: 'center'});
+    }
+
   };
 }
